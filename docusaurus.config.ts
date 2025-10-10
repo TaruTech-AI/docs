844f0cdc6dfba68fa133ca9b31e27964bd7c2b1e
Alfonso Ortiz Palma J <aortizpalma@gmail.com>
2025-10-10 17:56:53 +0300

    Changed broken link config from throw to warn to skip build errors.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,17 +15,18 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://TaruTech-AI.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'TaruTech-AI', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -98,7 +99,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'hhttps://github.com/TaruTech-AI',
+          href: 'https://github.com/TaruTech-AI',
           label: 'GitHub',
           position: 'right',
         },
